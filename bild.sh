d8b86549d9fa18d16e4aeb75283a68a681cae62c
mWalrus <hellkvistoskar@protonmail.com>
2022-09-14 20:06:56 +0200

    fix: correcting syntax mistakes and adding necessary steps

--- bild.sh
+++ bild.sh
@@ -77,16 +77,19 @@ fi
 
 new_task "Installing required packages:"
 list "nginx" "certbot" "python3-certbot-nginx" "gcc"
-# apt install nginx certbot python3-certbot-nginx gcc
+apt install nginx certbot python3-certbot-nginx gcc
 
 new_task "Making sure nginx is up and running"
-# systemctl start nginx && systemctl enable nginx
+systemctl start nginx && systemctl enable nginx
 
 new_task "Installing rustup"
-# curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+info "Adding cargo binaries to PATH"
+source "$HOME/.cargo/env"
 
 info "Installing rustup nightly toolchain"
-# rustup install nightly
+rustup install nightly
 
 DOMAIN_NAME="i.$(read_string "Enter your domain name (ex: your-domain.com)")"
 
@@ -96,70 +99,69 @@ if [ -f "$NGINX_CONF_FILE" ]; then
   info "Looks like $NGINX_CONF_FILE already exists"
   if [ grep -q "server_name $DOMAIN_NAME www.$DOMAIN_NAME" ]; then
     info "Updating conf file with required fields"
-    # sed "/server_name $DOMAIN_NAME www.$DOMAIN_NAME/a \tlocation / {\n\t\tproxy_pass http://127.0.0.1:1337;\n\t}" $NGINX_CONF_FILE
+    sed "/server_name $DOMAIN_NAME www.$DOMAIN_NAME/a \tlocation / {\n\t\tproxy_pass http://127.0.0.1:1337;\n\t}" $NGINX_CONF_FILE
   fi
 else
-  # touch "$NGINX_CONF_FILE"
-  # echo "server {" >> $NGINX_CONF_FILE
-  # echo "  server_name $DOMAIN_NAME www.$DOMAIN_NAME" >> $NGINX_CONF_FILE
-  # echo "  location / {" >> $NGINX_CONF_FILE
-  # echo "    proxy_pass http://127.0.0.1:1337;" >> $NGINX_CONF_FILE
-  # echo "  }" >> $NGINX_CONF_FILE
-  # echo "}" >> $NGINX_CONF_FILE
+  touch "$NGINX_CONF_FILE"
+  echo "server {" >> $NGINX_CONF_FILE
+  echo "  server_name $DOMAIN_NAME www.$DOMAIN_NAME;" >> $NGINX_CONF_FILE
+  echo "  location / {" >> $NGINX_CONF_FILE
+  echo "    proxy_pass http://127.0.0.1:1337;" >> $NGINX_CONF_FILE
+  echo "  }" >> $NGINX_CONF_FILE
+  echo "}" >> $NGINX_CONF_FILE
   
   info "Wrote server configuration to $(bold $NGINX_CONF_FILE)"
 fi
 
-info "Symlinking $(bold $NGINX_CONF_FILE) to $(bold "/etc/nginx/sites-enabled/$DOMAIN_NAME")"
-# ln -s $NGINX_CONF_FILE /etc/nginx/sites-enabled/$DOMAIN_NAME
+info "Symlinking $(bold $NGINX_CONF_FILE) to $(bold "/etc/nginx/sites-enabled/$DOMAIN_NAME.conf")"
+ln -s $NGINX_CONF_FILE /etc/nginx/sites-enabled/$DOMAIN_NAME.conf
 
-# FIXME: Ask first (certs might already be installed)
 new_task "Start certificate generation with certbot"
 if $(confirm "Do you want to generate certificates with certbot?" true); then
   info "Generating certs"
-  # certbot --nginx
+  certbot --nginx
 
   info "Reloading nginx"
-  # systemctl reload nginx
+  systemctl reload nginx
 else
   info "Skipping cert generation"
 fi
 
 
 new_task "Cloning down bild repo to $(bold "/var/www/bild")"
-# git clone https://gitlab.com/mWalrus/bild.git /var/www/bild
+git clone https://gitlab.com/mWalrus/bild.git /var/www/bild
 
 info "Changing $(bold "/var/www/bild")'s owner to www-data"
-# chown -R www-data: /var/www/bild
+chown -R www-data: /var/www/bild
 
 new_task "Compiling project binaries"
-# cd /var/www/bild && rustup run nightly cargo build --release
+cd /var/www/bild && rustup run nightly cargo build --release
 
 new_task "Adding systemd service $(bold /etc/systemd/system/bild-server.service)"
-# echo "
-# [Unit]
-# Description=My Rocket application for your-domain.com
+echo "
+[Unit]
+Description=My Rocket application for your-domain.com
 
-# [Service]
-# User=www-data
-# Group=www-data
-# # The user www-data should probably own that directory
-# WorkingDirectory=/var/www/bild
-# Environment=\"ROCKET_ENV=prod\"
-# Environment=\"ROCKET_ADDRESS=127.0.0.1\"
-# Environment=\"ROCKET_PORT=1337\"
-# Environment=\"ROCKET_LOG=critical\"
-# Environment=\"ROCKET_SERVER_URL=https://$DOMAIN_NAME\"
-# # Optional environment variable
-# # Environment=\"ROCKET_RATE_LIMIT=2\" # default is 2
-# ExecStart=/var/www/bild/target/release/bild-server
+[Service]
+User=www-data
+Group=www-data
+# The user www-data should probably own that directory
+WorkingDirectory=/var/www/bild
+Environment=\"ROCKET_ENV=prod\"
+Environment=\"ROCKET_ADDRESS=127.0.0.1\"
+Environment=\"ROCKET_PORT=1337\"
+Environment=\"ROCKET_LOG=critical\"
+Environment=\"ROCKET_SERVER_URL=https://$DOMAIN_NAME\"
+# Optional environment variable
+# Environment=\"ROCKET_RATE_LIMIT=2\" # default is 2
+ExecStart=/var/www/bild/target/release/bild-server
 
-# [Install]
-# WantedBy=multi-user.target
-# " > /etc/systemd/system/bild-server.service
+[Install]
+WantedBy=multi-user.target
+" > /etc/systemd/system/bild-server.service
 
 new_task "Starting up bild-server.service"
-# systemctl start bild-server && systemctl enable bild-server
+systemctl start bild-server && systemctl enable bild-server
 
 green_bold "Thats it! Below is some final information:"
 
